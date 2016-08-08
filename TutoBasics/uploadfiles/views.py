595dd94e933f6354f67280261cf2a7c98dd93f3e
kikeson <sabater@gmail.com>
2016-08-08 17:26:04 +0200

    Creado el boton de upload y carga

--- TutoBasics/uploadfiles/views.py
+++ TutoBasics/uploadfiles/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
+from uploadfiles.models import Document
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['file'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            # return HttpResponseRedirect(reverse('home_page'))
+    else:
+        form = UploadFileForm()
+        # A empty, unbound form
+
+    return render(
+        request,
+        'home.html',
+        {'form': form}
+    )

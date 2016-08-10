2fbc8458306352cbb2faac58378f414d7dea8056
kikeson <sabater@gmail.com>
2016-08-10 19:01:47 +0200

    Nuevos objetivos lista objetos

--- TutoBasics/functionalTests/tests.py
+++ TutoBasics/functionalTests/tests.py
@@ -7,6 +7,8 @@ import os
 import TutoBasics
 from uploadfiles.forms import UploadFileForm
 
+
+
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,6 +16,17 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def check_upload_file_ok(self,name):
+        file_element = self.browser.find_element_by_id('id_file')
+        file_element.send_keys(os.path.join(TutoBasics.settings.BASE_DIR,'documents',name))
+        self.browser.find_element_by_id('upload_submit').click()
+        file_element = self.browser.find_element_by_id('upload_ok')           
 
     def test_can_load_file_and_display_content(self):
         #Manuela visits page and loads correctly
@@ -29,9 +42,12 @@ class NewVisitorTest(LiveServerTestCase):
      
 
         #She upload her wonderful csv
-        name = 'prueba.csv'
-        file_element.send_keys(os.path.join(TutoBasics.settings.BASE_DIR,'documents',name))
-        self.browser.find_element_by_id('upload_submit').click()
+        #When she selects the file she gets a message that the loading was ok        
+        name1 = 'prueba.csv'
+        name2 = 'prueba2.csv'
+        self.check_upload_file_ok(name1)
+        self.check_upload_file_ok(name2)
+
 
         # f = open(os.path.join(TutoBasics.settings.BASE_DIR,'documents',name),"rb")
 
@@ -40,9 +56,16 @@ class NewVisitorTest(LiveServerTestCase):
         # form = UploadFileForm(data,file_data)
         # self.assertTrue(form.is_valid())
 
-        #When she selects the file she gets a message that the loading was ok
-        file_element = self.browser.find_element_by_id('upload_ok')   
-        #She gets the option to visualize a table with the contents
 
-        #If the file is too large only a sample is displayed
+
+
+        # She can see the list of files she has uploaded
+        self.check_for_row_in_list_table(name1)        
+        self.check_for_row_in_list_table(name2)
+
+        # She can select a file and load its content
+
+        # If the file is too large only a sample is displayed
+
+        # She gets the option to visualize the table graphically
 
